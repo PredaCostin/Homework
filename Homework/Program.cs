6b3d37d0c40e4cac54ca85e65048ce7b0e94818f
Costin <zoomless_la_familiaa@yahoo.com>
2018-12-01 17:50:51 +0200

    Add project files.

---
+++ Homework/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random nrRandom = new Random();
+            int number = nrRandom.Next(0, 100);
+
+            int nrIntrodus;
+
+            do
+            {
+                Console.WriteLine("Introduceti numarul: ");
+                nrIntrodus = int.Parse(Console.ReadLine());
+
+                if (nrIntrodus < number)
+                {
+                    Console.WriteLine("Numarul introdus este mai mic decat numarul generat.");
+                    Console.WriteLine();
+                }
+
+                else if (nrIntrodus == number)
+                {
+                    Console.WriteLine("Ati ghicit numarul!\nNumarul era " + number);
+                }
+
+                else if (nrIntrodus > number)
+                {
+                    Console.WriteLine("Numarul introdus este mai mare decat numarul generat.");
+                    Console.WriteLine();
+                }
+
+            } while (nrIntrodus != number);
+
+            Console.ReadKey();
+
+        }
+    }
+}
